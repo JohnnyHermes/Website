97b70ccda1a8fe2f24a7cfe16d7da0fb9d82c4e0
JohnnyHermes <52688651+JohnnyHermes@users.noreply.github.com>
2020-08-13 11:30:09 +0800

    Update ClickAnimation.js

--- blog/js/ClickAnimation.js
+++ blog/js/ClickAnimation.js
@@ -93,3 +93,7 @@ if(document.getElementById("clickCanvas")) {
         }();
     });
 }
+var canvas = document.createElement(‘div’);
+canvas.setAttribute(‘style’, ‘position: fixed;left: 0;top: 0;z-index: 999999999;pointer-events: none;’);
+canvas.setAttribute(‘id’, ‘clickCanvas’);
+document.getElementsByTagName(‘body’)[0].appendChild(canvas);
